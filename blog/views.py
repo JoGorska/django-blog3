4acf0f632bbe65869a22825bd4fda9b7bcf2b46f
Joanna Gorska <gorska919@gmail.com>
2021-12-07 20:51:50 +0000

    Add form for user comments to the post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
